06a94983040039ad286842c349ef308eb4632a70
Branislav Blazek <blazek@pap.sk>
2021-11-27 22:09:23 +0100

    logic works

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 const type = {
     CLICK_ON_MORE_BUTTON: 'clickOnMoreButton',
+    SCRAPE_COMMENTS: 'scrapeComments',
     CONSOLE_LOG: 'consoleLog'
 }
 
@@ -7,28 +8,6 @@ const answer = {
     OK: 'ok',
     FAIL: 'fail'
 };
-
-let buttonClicks = 0;
-
-const action = {
-    [type.CLICK_ON_MORE_BUTTON]: () => sendMessage(
-        type.CLICK_ON_MORE_BUTTON,
-        null,
-        payload => {
-            if (payload === answer.OK) {
-                buttonClicks += 1;
-                consoleLog(`Button was clicked for ${buttonClicks} times.`);
-                clickOnMoreButton();
-            } else if (payload === answer.FAIL) { consoleLog(`Problem with Button at ${buttonClicks} click.`) }
-            else { consoleLog(`Unregistred error with Button at ${buttonClicks} click.`) }
-
-        },
-    ),
-    [type.CONSOLE_LOG]: payload => sendMessage(type.CONSOLE_LOG, payload)
-}
-
-const clickOnMoreButton = () => action[type.CLICK_ON_MORE_BUTTON]();
-const consoleLog = payload => action[type.CONSOLE_LOG](payload);
 
 const sendMessage = (type, payload, callback = () => { }) => {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
@@ -41,4 +20,51 @@ const sendMessage = (type, payload, callback = () => { }) => {
     });
 };
 
-chrome.action.onClicked.addListener(() => clickOnMoreButton());
+const action = {
+    [type.CLICK_ON_MORE_BUTTON]: () => sendMessage(
+        type.CLICK_ON_MORE_BUTTON,
+        null,
+        payload => {
+            if (payload === answer.OK) {
+                consoleLog(`Button was succesfully clicked.`);
+            } else if (payload === answer.FAIL) {
+                consoleLog(`Problem with button click.`)
+                chrome.alarms.clear('buttonAlarm')
+                consoleLog('Clearing buttonAlarm alarm.')
+                scrapeComments()
+            }
+            else { consoleLog(`Unregistred error with button click.`) }
+
+        },
+    ),
+    [type.SCRAPE_COMMENTS]: () => sendMessage(
+        type.SCRAPE_COMMENTS,
+        null,
+        payload => {
+            if (payload.length) {
+                consoleLog('Succesfully scarped comments!');
+                consoleLog(payload);
+            } else {
+                consoleLog('Problem with scraping...')
+            }
+        },
+    ),
+    [type.CONSOLE_LOG]: payload => sendMessage(type.CONSOLE_LOG, payload)
+}
+
+const clickOnMoreButton = () => action[type.CLICK_ON_MORE_BUTTON]();
+const scrapeComments = () => action[type.SCRAPE_COMMENTS]();
+const consoleLog = payload => action[type.CONSOLE_LOG](payload);
+
+chrome.alarms.onAlarm.addListener(() => {
+    consoleLog('Trying to click a button.')
+    clickOnMoreButton()
+})
+
+const handleInit = () => {
+    consoleLog('Initializing extension...');
+    consoleLog('Creating buttonAlarm alarm.');
+    chrome.alarms.create('buttonAlarm', { periodInMinutes: 0.01 })
+}
+
+chrome.action.onClicked.addListener(handleInit);
--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 const type = {
     CLICK_ON_MORE_BUTTON: 'clickOnMoreButton',
+    SCRAPE_COMMENTS: 'scrapeComments',
     CONSOLE_LOG: 'consoleLog'
 }
 
@@ -12,7 +13,6 @@ const clickOnMoreButton = () => {
     return new Promise(async (resolve, reject) => {
         const getScript = label => document.querySelectorAll(`[aria-label="${label}"]`)[0]?.parentElement?.parentElement
         const button = await getScript("Načítať ďalšie komentáre") || await getScript('Load more comments');
-        console.log(button);
         if (button) {
             button.click();
             resolve(answer.OK)
@@ -20,6 +20,22 @@ const clickOnMoreButton = () => {
     });
 }
 
+const scrapeFunction = scrapeFn => {
+    return new Promise(async (resolve, reject) => {
+        const data = [...document.querySelectorAll('.Mr508')].map(comments => {
+            const comment = comments.querySelector('.C4VMK');
+            const user = comment.querySelector('a').textContent;
+            const text = comment.querySelector('span:nth-child(2)').textContent;
+            return {
+                author: user,
+                comment: text,
+            };
+        })
+        if (data) {
+            resolve(data)
+        } else reject(answer.FAIL)
+    });
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch (request.type) {
@@ -28,6 +44,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             break;
         case type.CLICK_ON_MORE_BUTTON:
             clickOnMoreButton().then(sendResponse).catch(sendResponse);
+            return true;
+        case type.SCRAPE_COMMENTS:
+            scrapeFunction().then(sendResponse).catch(sendResponse);
             return true;
         default:
             console.log('Invalid event type!');
